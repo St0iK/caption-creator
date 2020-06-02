fc019f1d172b27cbe8a6a035c040e262ce81a192
Dimitris Stoikidis <dimitrios.stoikidis@sky.uk>
2020-06-02 08:36:40 +0100

    Fix comments

--- backend/src/loaders/express.ts
+++ backend/src/loaders/express.ts
@@ -4,13 +4,14 @@ import cors from 'cors';
 import routes from '../api';
 import config from '../config';
 export default ({ app }: { app: express.Application }) => {
+
   /**
    * Health Check endpoints
-   * @TODO Explain why they are here
    */
   app.get('/status', (req, res) => {
     res.status(200).end();
   });
+
   app.head('/status', (req, res) => {
     res.status(200).end();
   });
@@ -19,14 +20,9 @@ export default ({ app }: { app: express.Application }) => {
   // It shows the real origin IP in the heroku or Cloudwatch logs
   app.enable('trust proxy');
 
-  // The magic package that prevents frontend developers going nuts
-  // Alternate description:
   // Enable Cross Origin Resource Sharing to all origins by default
   app.use(cors());
 
-  // Some sauce that always add since 2014
-  // "Lets you use HTTP verbs such as PUT or DELETE in places where the client doesn't support it."
-  // Maybe not needed anymore ?
   app.use(require('method-override')());
 
   // Middleware that transforms the raw string of req.body into json
