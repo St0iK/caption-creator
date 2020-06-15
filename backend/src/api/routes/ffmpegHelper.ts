cd1413db16c300c6870d10ba7460c0524562ab12
Dimitris Stoikidis <jstoikidis@gmail.com>
2020-06-15 09:39:13 +0100

    POC on update with convert to audio

---
+++ backend/src/api/routes/ffmpegHelper.ts
@@ -0,0 +1,30 @@
+import Logger from '../../loaders/logger';
+import ffmpeg from 'fluent-ffmpeg';
+import ffmpegStatic from 'ffmpeg-static';
+
+Logger.info('FFMPEG is about to run !');
+
+const convertVideo = (path) => {
+  return new Promise((resolve, reject) => {
+    Logger.info(ffmpegStatic);
+    // ffmpeg('./file_example_MOV_1920_2_2MB.mov')
+    ffmpeg(path)
+      .setFfmpegPath(ffmpegStatic)
+      .audioChannels(1)
+      .audioFrequency(16000)
+      .format('flac')
+      .saveToFile('./export.flac')
+      .on('progress', (progress) => {
+        Logger.info(`[ffmpeg] ${JSON.stringify(progress)}`);
+      })
+      .on('error', (err) => {
+        Logger.info(`[ffmpeg] error: ${err.message}`);
+      })
+      .on('end', () => {
+        Logger.verbose('[ffmpeg] finished');
+      })
+
+  });
+}
+
+export default convertVideo;
--- backend/src/api/routes/file-upload.ts
+++ backend/src/api/routes/file-upload.ts
@@ -2,28 +2,30 @@ import Logger from '../../loaders/logger';
 import { Router, Request, Response } from 'express';
 const route = Router();
 import multer from 'multer';
+var upload = multer({ dest: 'uploads/' })
 import MulterGoogleCloudStorage from 'multer-google-storage';
 import config from '../../config';
 import ffmpeg from 'fluent-ffmpeg';
 import ffmpegStatic from 'ffmpeg-static';
+import convertVideo from './ffmpegHelper';
 const fileUpload = require('express-fileupload');
 
 
-const uploadHandler = multer({
-  storage: new MulterGoogleCloudStorage({
-    email: config.googleCloudStorage.email,
-    keyFilename: config.googleCloudStorage.videoUpload.keyFilename,
-    projectId: config.googleCloudStorage.videoUpload.projectId,
-    bucket: config.googleCloudStorage.videoUpload.bucket,
-  })
-});
+// const uploadHandler = multer({
+//   storage: new MulterGoogleCloudStorage({
+//     email: config.googleCloudStorage.email,
+//     keyFilename: config.googleCloudStorage.videoUpload.keyFilename,
+//     projectId: config.googleCloudStorage.videoUpload.projectId,
+//     bucket: config.googleCloudStorage.videoUpload.bucket,
+//   })
+// });
 
 export default (app: Router) => {
 
   app.use('/upload', route);
   app.use(fileUpload());
 
-  route.post('/video', uploadHandler.any(), async (req: Request, res: Response, next: any) => {
+  route.post('/video', upload.any(), async (req: Request, res: Response, next: any) => {
     if (!req.files || Object.keys(req.files).length === 0) {
       return res.status(400).send('No files were uploaded.');
     }
@@ -34,25 +36,11 @@ export default (app: Router) => {
     Logger.info(path);
 
     try {
-      await ffmpeg(path)
-        .setFfmpegPath(ffmpegStatic.path)
-        .audioChannels(1)
-        .audioFrequency(16000)
-        .format('flac')
-        .output('./yoyoyo')
-        .on('progress', (progress) => {
-          Logger.info(`[ffmpeg] ${JSON.stringify(progress)}`);
-        })
-        .on('error', (err) => {
-          Logger.info(`[ffmpeg] error: ${err.message}`);
-        })
-        .on('end', () => {
-          Logger.verbose('[ffmpeg] finished');
-        })
-    } catch (e) {
-      Logger.info(e);
+      await convertVideo(path);
+    } catch {
+      Logger.error('Oppps!');
     }
 
-    res.json(file);
+
   });
 };
