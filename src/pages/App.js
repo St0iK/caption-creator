95940d04973ab39e65a5c60bb16c850e1ad460e3
Dimitris Stoikidis <dimitrios.stoikidis@sky.uk>
2020-06-25 10:25:43 +0100

    Clean up

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,22 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import withRoot from "../withRoot";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import { Paper, makeStyles } from "@material-ui/core/";
 import VTTEditor from "../components/Editor/VTTEditor";
 import Player from "../components/Player/Player";
-import axios from 'axios';
-import { getCuesFromWords } from '../services/VttGenerator';
-import { FilePond, registerPlugin } from "react-filepond"
-import "filepond/dist/filepond.min.css"
-import FilePondPluginImageExifOrientation from "filepond-plugin-image-exif-orientation"
-import FilePondPluginImagePreview from "filepond-plugin-image-preview"
-import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css"
-import { useSelector, useDispatch } from "react-redux";
-import { onChangeCues } from "../store/actions/cueActions";
-
-// Register the plugins
-registerPlugin(FilePondPluginImageExifOrientation, FilePondPluginImagePreview);
 
 const useStyles = makeStyles({
   root: {
